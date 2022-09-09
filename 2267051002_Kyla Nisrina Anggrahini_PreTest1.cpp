a347a619045cb45620b879a0381498bc9059b2db
KYLA NISRINA ANGGRAHINI <88182323+kylanisrina@users.noreply.github.com>
2022-09-09 15:25:29 +0700

    Add files via upload

---
+++ 2267051002_Kyla Nisrina Anggrahini_PreTest1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string nama;
+	string umur;
+	string harapan;
+	
+	cout<<"Kyla Nisrina Anggrahini";
+	getline(cin,nama);
+	
+	cout<<"18 Tahun";
+	cin.ignore(1,'\n');
+	
+	cout<<"Lebih Baik Dari Sebelumnya";
+	getline(cin,harapan);
+	
+	
+	cout<<"nama";
+	cout<<"umur";
+	cout<<"harapan";
+	
+	return 0;
+}
